86a7f75d0287f3d258b2e5741067d123c50502ec
Kenkyoo <124638519+Kenkyoo@users.noreply.github.com>
2025-02-25 05:30:21 -0300

    Update main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -14,7 +14,8 @@ createRoot(document.getElementById("root")!).render(
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
-      cacheLocation="localstorage"
+        cacheLocation="localstorage"
+        useRefreshTokens={true}
     >
       <QueryClientProvider client={queryClient}>
         <App />
